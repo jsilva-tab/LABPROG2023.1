0a1f6d307a2413d8a5ca47652fa2197d9d0a69e7
J. Silva <112393484+jsilva-tab@users.noreply.github.com>
2023-02-15 09:38:35 -0300

    Update ex-ternario.c

--- ex-ternario.c
+++ ex-ternario.c
@@ -10,6 +10,8 @@ int main(){
     // 'result' é a variavel que vai receber a condição do if, em seguida tem-se a condição
     //depois do ? é o if e depois do : é o else
     // ou result *= valor%2==0?2:1;
+    // versão mais sofisticada:
+    // x *= (x%2)?1:2;
 
     printf("%d\n",result);
     return 0;
